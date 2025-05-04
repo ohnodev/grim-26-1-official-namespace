ba1ee9b08b2ceb066e77c9f4872d3d39fc2e866a
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-05-04 11:55:54 -0600

    don't let people create ghost blocks by intentionally flagging

--- common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsH.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsH.java
@@ -1,21 +1,22 @@
 package ac.grim.grimac.checks.impl.badpackets;
 
-import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
-import ac.grim.grimac.checks.type.PacketCheck;
+import ac.grim.grimac.checks.type.BlockBreakCheck;
+import ac.grim.grimac.checks.type.BlockPlaceCheck;
 import ac.grim.grimac.player.GrimPlayer;
+import ac.grim.grimac.utils.anticheat.update.BlockBreak;
+import ac.grim.grimac.utils.anticheat.update.BlockPlace;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.protocol.player.DiggingAction;
-import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerBlockPlacement;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerDigging;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientUseItem;
 
 @CheckData(name = "BadPacketsH", description = "Sent unexpected sequence id", experimental = true)
-public class BadPacketsH extends Check implements PacketCheck {
+public class BadPacketsH extends BlockPlaceCheck {
     private int lastSequence;
     private final boolean isSupportedVersion = player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_19) && PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_19);
 
@@ -26,14 +27,13 @@ public class BadPacketsH extends Check implements PacketCheck {
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
         if (isSupportedVersion) {
-            if (event.getPacketType() == PacketType.Play.Client.PLAYER_BLOCK_PLACEMENT) {
-                handleSequenceId(new WrapperPlayClientPlayerBlockPlacement(event).getSequence(), event);
-            }
-
             if (event.getPacketType() == PacketType.Play.Client.PLAYER_DIGGING) {
                 WrapperPlayClientPlayerDigging packet = new WrapperPlayClientPlayerDigging(event);
                 if (packet.getAction() == DiggingAction.START_DIGGING || packet.getAction() == DiggingAction.FINISHED_DIGGING) {
-                    handleSequenceId(packet.getSequence(), event);
+                    if (shouldCancel(packet.getSequence())) {
+                        event.setCancelled(true);
+                        player.onPacketCancel();
+                    }
                 } else if (packet.getSequence() != 0 && packet.getAction() == DiggingAction.CANCELLED_DIGGING
                         && flagAndAlert("expected=0, id=" + packet.getSequence()) && shouldModifyPackets()) {
                     event.setCancelled(true);
@@ -41,21 +41,46 @@ public class BadPacketsH extends Check implements PacketCheck {
                 }
             }
 
-            if (event.getPacketType() == PacketType.Play.Client.USE_ITEM) {
-                handleSequenceId(new WrapperPlayClientUseItem(event).getSequence(), event);
-            }
-        }
-    }
-
-    public void handleSequenceId(int sequence, PacketReceiveEvent event) {
-        if (sequence != lastSequence + 1) {
-            if (flagAndAlert("expected=" + (lastSequence + 1) + ", id=" + sequence) && shouldModifyPackets()) {
+            if (event.getPacketType() == PacketType.Play.Client.USE_ITEM
+                    && shouldCancel(new WrapperPlayClientUseItem(event).getSequence())) {
                 event.setCancelled(true);
                 player.onPacketCancel();
             }
         }
+    }
+
+    @Override
+    public void onBlockPlace(BlockPlace place) {
+        if (shouldCancel(place.sequence) && shouldCancel()) {
+            place.resync();
+        }
+    }
+
+    @Override
+    public void onBlockBreak(BlockBreak blockBreak) {
+        switch (blockBreak.action) {
+            case START_DIGGING, FINISHED_DIGGING -> {
+                if (shouldCancel(blockBreak.sequence)) {
+                    blockBreak.cancel();
+                }
+            }
+            case CANCELLED_DIGGING -> { // other actions will be checked by BadPacketsL
+                if (blockBreak.sequence != 0 && flagAndAlert("expected=0, id=" + blockBreak.sequence) && shouldModifyPackets()) {
+                    blockBreak.cancel();
+                }
+            }
+        }
+    }
+
+    public boolean shouldCancel(int sequence) {
+        if (sequence != lastSequence + 1) {
+            if (flagAndAlert("expected=" + (lastSequence + 1) + ", id=" + sequence) && shouldModifyPackets()) {
+                return true;
+            }
+        }
 
         lastSequence = sequence;
+        return false;
     }
 
     public void onWorldChange() {
