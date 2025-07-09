0ccd03a669eada7aede86ff7b6c77c845bd69dcf
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-07-09 12:49:12 -0600

    ExploitC fail-fast page parsing

--- common/src/main/java/ac/grim/grimac/utils/payload/PayloadBookEdit.java
+++ common/src/main/java/ac/grim/grimac/utils/payload/PayloadBookEdit.java
@@ -2,6 +2,9 @@ package ac.grim.grimac.utils.payload;
 
 import com.github.retrooper.packetevents.protocol.item.ItemStack;
 
+/**
+ * Payload wrapper for {@code MC|BEdit} and {@code MC|BSign}
+ */
 public record PayloadBookEdit(ItemStack itemStack) {
     public static final PayloadCodec<PayloadBookEdit> CODEC = new PayloadCodec<>(
             wrapper -> new PayloadBookEdit(wrapper.readItemStack()),
