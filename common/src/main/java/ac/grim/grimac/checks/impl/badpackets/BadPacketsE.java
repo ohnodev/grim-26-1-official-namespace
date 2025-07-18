2ad98188f60daeadcc270fe3ec1bdb7aa45b4c9a
Axionize <154778082+Axionize@users.noreply.github.com>
2025-07-17 23:55:22 -0400

    Temporary fix for BadPacketsE not working on vehicles

--- common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsE.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsE.java
@@ -4,7 +4,9 @@ import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
+import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
@@ -13,6 +15,7 @@ import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPl
 public class BadPacketsE extends Check implements PacketCheck {
     private int noReminderTicks;
     private final int maxNoReminderTicks = player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_8) ? 20 : 19;
+    private final boolean isViaPleaseStopUsingProtocolHacksOnYourServer = player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_21_2) || PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_21_2);
 
     public BadPacketsE(GrimPlayer player) {
         super(player);
@@ -27,7 +30,8 @@ public class BadPacketsE extends Check implements PacketCheck {
             if (++noReminderTicks > maxNoReminderTicks) {
                 flagAndAlert("ticks=" + noReminderTicks);
             }
-        } else if (event.getPacketType() == PacketType.Play.Client.STEER_VEHICLE) {
+        } else if (event.getPacketType() == PacketType.Play.Client.STEER_VEHICLE
+                || (isViaPleaseStopUsingProtocolHacksOnYourServer && player.inVehicle())) {
             noReminderTicks = 0; // Exempt vehicles
         }
     }
