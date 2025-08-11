6787d1bcd175ca60cb905307dfe5e95aa335681d
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-08-11 14:28:09 -0600

    split isJumping and lastJumping into separate declarations

--- common/src/main/java/ac/grim/grimac/player/GrimPlayer.java
+++ common/src/main/java/ac/grim/grimac/player/GrimPlayer.java
@@ -275,7 +275,8 @@ public class GrimPlayer implements GrimUser {
     public final LongSet visitedBlocks = new LongOpenHashSet();
     private @Nullable UserConnection viaUserConnection;
     public boolean wasLastPredictionCompleteChecked;
-    public boolean isJumping = false, lastJumping = false;
+    public boolean isJumping;
+    public boolean lastJumping;
 
     public GrimPlayer(@NonNull User user) {
         this.user = user;
