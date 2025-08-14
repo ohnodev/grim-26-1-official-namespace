38ccc559ebcfbc2519d5d940276859fb2125eec5
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-08-14 17:38:51 -0600

    fix ChatA/ChatB falses from 31107ec4b

--- common/src/main/java/ac/grim/grimac/checks/impl/chat/ChatB.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/chat/ChatB.java
@@ -4,7 +4,9 @@ import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
+import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientChatCommand;
@@ -23,7 +25,7 @@ public class ChatB extends Check implements PacketCheck {
     public void onPacketReceive(PacketReceiveEvent event) {
         if (event.getPacketType() == PacketType.Play.Client.CHAT_MESSAGE) {
             String message = new WrapperPlayClientChatMessage(event).getMessage();
-            if (message.isEmpty() || !message.trim().equals(message) || message.startsWith("/") && player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_19)) {
+            if (message.isEmpty() || !message.trim().equals(message) || message.startsWith("/") && PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_19)) {
                 if (flagAndAlert("message=" + message)) {
                     event.setCancelled(true);
                     player.onPacketCancel();
