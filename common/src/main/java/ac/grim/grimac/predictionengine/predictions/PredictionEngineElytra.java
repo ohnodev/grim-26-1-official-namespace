28d83206c1cd36daecba86e5ee1f606c799205ab
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-08-16 17:24:05 -0600

    more readable PredictionEngineElytra.applyInputsToVelocityPossibilities

--- common/src/main/java/ac/grim/grimac/predictionengine/predictions/PredictionEngineElytra.java
+++ common/src/main/java/ac/grim/grimac/predictionengine/predictions/PredictionEngineElytra.java
@@ -57,17 +57,14 @@ public class PredictionEngineElytra extends PredictionEngine {
     @Override
     public List<VectorData> applyInputsToVelocityPossibilities(GrimPlayer player, Set<VectorData> possibleVectors, float speed) {
         List<VectorData> results = new ArrayList<>();
-        Vector3dm currentLook = ReachUtils.getLook(player, player.xRot, player.yRot);
-        player.trigHandler.toggleShitMath();
-        Vector3dm shitmathLook = ReachUtils.getLook(player, player.xRot, player.yRot);
-        player.trigHandler.toggleShitMath();
 
-        for (VectorData data : possibleVectors) {
-            // We must bruteforce Optifine ShitMath
-            for (int shitmath = 0; shitmath <= 1; shitmath++, player.trigHandler.toggleShitMath()) {
-                for (int applyStuckSpeed = 1; applyStuckSpeed >= 0; applyStuckSpeed--) {
-                    if (applyStuckSpeed == 0 && player.isForceStuckSpeed()) break;
-                    Vector3dm elytraResult = getElytraMovement(player, data.vector.clone(), shitmath == 1 ? shitmathLook : currentLook);
+        // We must bruteforce Optifine ShitMath
+        for (int shitmath = 0; shitmath <= 1; shitmath++, player.trigHandler.toggleShitMath()) {
+            Vector3dm currentLook = ReachUtils.getLook(player, player.xRot, player.yRot);
+            for (int applyStuckSpeed = 1; applyStuckSpeed >= 0; applyStuckSpeed--) {
+                if (applyStuckSpeed == 0 && player.isForceStuckSpeed()) break;
+                for (VectorData data : possibleVectors) {
+                    Vector3dm elytraResult = getElytraMovement(player, data.vector.clone(), currentLook);
                     if (applyStuckSpeed != 0) elytraResult.multiply(player.stuckSpeedMultiplier);
                     elytraResult.multiply(new Vector3dm(0.99F, 0.98F, 0.99F));
                     VectorData modified = data.returnNewModified(elytraResult, VectorData.VectorType.InputResult);
