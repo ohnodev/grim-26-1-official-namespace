0e6d116ec6da3827ed6a7d435df490c1625b9491
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-10-07 15:35:30 -0600

    nullability annotations & deobfing

--- common/src/main/java/ac/grim/grimac/utils/nmsutil/JumpPower.java
+++ common/src/main/java/ac/grim/grimac/utils/nmsutil/JumpPower.java
@@ -8,12 +8,13 @@ import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.protocol.potion.PotionTypes;
 import com.github.retrooper.packetevents.util.Vector3d;
 import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.OptionalInt;
 
 @UtilityClass
 public class JumpPower {
-    public static void jumpFromGround(GrimPlayer player, Vector3dm vector) {
+    public static void jumpFromGround(@NotNull GrimPlayer player, @NotNull Vector3dm vector) {
         float jumpPower = getJumpPower(player);
 
         final OptionalInt jumpBoost = player.compensatedEntities.getPotionLevelForPlayer(PotionTypes.JUMP_BOOST);
@@ -32,11 +33,11 @@ public class JumpPower {
         }
     }
 
-    public static float getJumpPower(GrimPlayer player) {
+    public static float getJumpPower(@NotNull GrimPlayer player) {
         return (float) player.compensatedEntities.self.getAttributeValue(Attributes.JUMP_STRENGTH) * getPlayerJumpFactor(player);
     }
 
-    public static float getPlayerJumpFactor(GrimPlayer player) {
+    public static float getPlayerJumpFactor(@NotNull GrimPlayer player) {
         return BlockProperties.onHoneyBlock(player, player.mainSupportingBlockData, new Vector3d(player.lastX, player.lastY, player.lastZ)) ? 0.5f : 1f;
     }
 }
